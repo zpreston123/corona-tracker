1706b439db8a0e6d6bcb6a2497511a4f111df6a6
Zach Preston <zpdrums@comcast.net>
2020-04-15 19:27:50 -0400

    Updates

---
+++ precache-manifest.c08a17a35acdafedbc8893df0db52120.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94b01538382124bbaa996f1821a35d1a",
+    "url": "/corona-tracker/index.html"
+  },
+  {
+    "revision": "8eefc87b03a28d3eb4ce",
+    "url": "/corona-tracker/static/css/main.1cd01c98.chunk.css"
+  },
+  {
+    "revision": "107611f6f36d44e181a6",
+    "url": "/corona-tracker/static/js/2.0dd72c05.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/corona-tracker/static/js/2.0dd72c05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8eefc87b03a28d3eb4ce",
+    "url": "/corona-tracker/static/js/main.50f9e47e.chunk.js"
+  },
+  {
+    "revision": "69a10effee46d655a76b",
+    "url": "/corona-tracker/static/js/runtime-main.b1278e06.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/corona-tracker/static/media/image.d7265326.png"
+  }
+]);
