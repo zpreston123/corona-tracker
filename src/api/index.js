05b5e681fe4bc16f0cfef657ab18ba96b3435b07
Zach Preston <zpdrums@comcast.net>
2020-05-09 16:18:22 -0400

    refactor api

--- src/api/index.js
+++ src/api/index.js
@@ -14,7 +14,7 @@ export const fetchData = async (country) => {
 
 		return { confirmed, recovered, deaths, lastUpdate };
 	} catch (error) {
-		console.log(error);
+		return error;
 	}
 }
 
@@ -22,15 +22,9 @@ export const fetchDailyData = async () => {
 	try {
 		const { data } = await axios.get(`${url}/daily`);
 
-		const modifiedData = data.map((dailyData) => ({
-			confirmed: dailyData.confirmed.total,
-			deaths: dailyData.deaths.total,
-			date: dailyData.reportDate
-		}));
-
-		return modifiedData;
+		return data.map(({ confirmed, deaths, reportDate: date }) => ({ confirmed: confirmed.total, deaths: deaths.total, date }));
 	} catch (error) {
-		console.log(error);
+		return error;
 	}
 }
 
@@ -40,6 +34,6 @@ export const fetchCountries = async () => {
 
 		return countries.map((country) => country.name);
 	} catch (error) {
-		console.log(error);
+		return error;
 	}
 }
