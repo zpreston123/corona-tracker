2e4f8e6453eebdec2acc8bce935d740477d32108
Zach Preston <zpdrums@comcast.net>
2021-11-02 14:43:50 -0400

    update footer text

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
     return (
         <div className={styles.footer}>
             <Typography variant="body1" component="h2">
-                US data sourced from <Link href="https://www.nytimes.com/interactive/2021/us/covid-cases.html">New York Times</Link> and <Link href="https://www.worldometers.info/coronavirus/">Worldometer</Link>.<br />
+                US data sourced from <Link href="https://www.nytimes.com/interactive/2021/us/covid-cases.html">New York Times</Link>, <Link href="https://www.worldometers.info/coronavirus/">Worldometer</Link>, and <Link href="https://ourworldindata.org/coronavirus/">Our World in Data</Link>.<br />
                 Global data sourced from <Link href="https://coronavirus.jhu.edu/">John Hopkins University CSSE</Link> and <Link href="https://www.worldometers.info/coronavirus/">Worldometer</Link>.
             </Typography>
         </div>
